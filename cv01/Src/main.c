76b092c0e435b7f86204da56eacfbe812d388f91
xcerma37@protonmail.com <xcerma37@protonmail.com>
2020-10-08 21:36:52 +0200

    cv01 all

--- cv01/Src/main.c
+++ cv01/Src/main.c
@@ -17,12 +17,27 @@
  ******************************************************************************
  */
 
+#include "stm32f0xx.h"
+
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
 int main(void)
 {
+    //static const uint8_t pole[32] = {1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0};
+	static const uint32_t pole = 0b10101001110111011100101010000000;
+
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= GPIO_MODER_MODER5_0;
     /* Loop forever */
-	for(;;);
+	for (;;){
+
+		for (uint8_t i = 0; i < 32; i++){
+			if (pole & (1 << (32 - i))) GPIOA->BSRR = (1<<5); // toggle
+			else GPIOA->BRR = (1<<5);
+
+			for (volatile uint32_t i = 0; i < 100000; i++) {}
+		}
+	}
 }
